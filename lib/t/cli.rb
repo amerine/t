028052780862b10fbd2a6186a840b86310ca081e
Mark Turner <mark@amerine.net>
2012-05-28 13:10:08 -0700

    Moving common sorts to a group in locale

--- lib/t/cli.rb
+++ lib/t/cli.rb
@@ -122,7 +122,7 @@ module T
     method_option "csv", :aliases => "-c", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.csv")
     method_option "long", :aliases => "-l", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.long")
     method_option "number", :aliases => "-n", :type => :numeric, :default => DEFAULT_NUM_RESULTS, :desc => I18n.t("tasks.common_options.number")
-    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.reverse")
+    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.reverse")
     def direct_messages
       count = options['number'] || DEFAULT_NUM_RESULTS
       direct_messages = collect_with_count(count) do |opts|
@@ -155,7 +155,7 @@ module T
     method_option "csv", :aliases => "-c", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.csv")
     method_option "long", :aliases => "-l", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.long")
     method_option "number", :aliases => "-n", :type => :numeric, :default => DEFAULT_NUM_RESULTS, :desc => I18n.t("tasks.common_options.number")
-    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.reverse")
+    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.reverse")
     def direct_messages_sent
       count = options['number'] || DEFAULT_NUM_RESULTS
       direct_messages = collect_with_count(count) do |opts|
@@ -186,16 +186,16 @@ module T
 
     desc "groupies [USER]", I18n.t('tasks.groupies.desc')
     method_option "csv", :aliases => "-c", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.csv')
-    method_option "favorites", :aliases => "-v", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.favorites')
-    method_option "followers", :aliases => "-f", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.followers')
-    method_option "friends", :aliases => "-e", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.friends')
+    method_option "favorites", :aliases => "-v", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.favorites')
+    method_option "followers", :aliases => "-f", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.followers')
+    method_option "friends", :aliases => "-e", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.friends')
     method_option "id", :aliases => "-i", :type => "boolean", :default => false, :desc => I18n.t('tasks.common_options.id')
-    method_option "listed", :aliases => "-d", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.listed')
+    method_option "listed", :aliases => "-d", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.listed')
     method_option "long", :aliases => "-l", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.long')
-    method_option "posted", :aliases => "-p", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.posted')
-    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.reverse')
-    method_option "tweets", :aliases => "-t", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.tweets')
-    method_option "unsorted", :aliases => "-u", :type => :boolean, :default => false, :desc => I18n.t('tasks.groupies.unsorted')
+    method_option "posted", :aliases => "-p", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.posted')
+    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.reverse')
+    method_option "tweets", :aliases => "-t", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.tweets')
+    method_option "unsorted", :aliases => "-u", :type => :boolean, :default => false, :desc => I18n.t('tasks.common_options.sorts.unsorted')
     def groupies(user=nil)
       if user
         require 't/core_ext/string'
@@ -324,7 +324,7 @@ module T
     method_option "id", :aliases => "-i", :type => "boolean", :default => false, :desc => I18n.t("tasks.common_options.id")
     method_option "long", :aliases => "-l", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.long")
     method_option "number", :aliases => "-n", :type => :numeric, :default => DEFAULT_NUM_RESULTS, :desc => I18n.t("tasks.common_options.number")
-    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.reverse")
+    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.reverse")
     def favorites(user=nil)
       if user
         require 't/core_ext/string'
@@ -369,16 +369,16 @@ module T
 
     desc "followings [USER]", I18n.t("tasks.followings.desc")
     method_option "csv", :aliases => "-c", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.csv")
-    method_option "favorites", :aliases => "-v", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.favorites")
-    method_option "followers", :aliases => "-f", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.followers")
-    method_option "friends", :aliases => "-e", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.friends")
+    method_option "favorites", :aliases => "-v", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.favorites")
+    method_option "followers", :aliases => "-f", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.followers")
+    method_option "friends", :aliases => "-e", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.friends")
     method_option "id", :aliases => "-i", :type => "boolean", :default => false, :desc => I18n.t("tasks.common_options.id")
-    method_option "listed", :aliases => "-d", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.listed")
+    method_option "listed", :aliases => "-d", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.listed")
     method_option "long", :aliases => "-l", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.long")
-    method_option "posted", :aliases => "-p", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.posted")
-    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.reverse")
-    method_option "tweets", :aliases => "-t", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.tweets")
-    method_option "unsorted", :aliases => "-u", :type => :boolean, :default => false, :desc => I18n.t("tasks.followings.unsorted")
+    method_option "posted", :aliases => "-p", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.posted")
+    method_option "reverse", :aliases => "-r", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.reverse")
+    method_option "tweets", :aliases => "-t", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.tweets")
+    method_option "unsorted", :aliases => "-u", :type => :boolean, :default => false, :desc => I18n.t("tasks.common_options.sorts.unsorted")
     def followings(user=nil)
       if user
         require 't/core_ext/string'
