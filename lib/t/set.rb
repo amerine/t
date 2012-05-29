734bb2ab9df9b8b3aec806e2d1ba277c9738be31
Mark Turner <mark@amerine.net>
2012-05-29 00:44:18 -0700

    Migrate set subcommands to locales.

--- lib/t/set.rb
+++ lib/t/set.rb
@@ -13,45 +13,45 @@ module T
       @rcfile = RCFile.instance
     end
 
-    desc "active SCREEN_NAME [CONSUMER_KEY]", "Set your active account."
+    desc "active SCREEN_NAME [CONSUMER_KEY]", I18n.t("tasks.set.active.desc")
     def active(screen_name, consumer_key=nil)
       require 't/core_ext/string'
       screen_name = screen_name.strip_ats
       @rcfile.path = options['profile'] if options['profile']
       consumer_key = @rcfile[screen_name].keys.last if consumer_key.nil?
       @rcfile.active_profile = {'username' => screen_name, 'consumer_key' => consumer_key}
-      say "Active account has been updated."
+      say I18n.t("tasks.set.active.success")
     end
     map %w(default) => :active
 
-    desc "bio DESCRIPTION", "Edits your Bio information on your Twitter profile."
+    desc "bio DESCRIPTION", I18n.t("tasks.set.bio.desc")
     def bio(description)
       client.update_profile(:description => description)
-      say "@#{@rcfile.active_profile[0]}'s bio has been updated."
+      say I18n.t("tasks.set.bio.success", :profile => @rcfile.active_profile[0])
     end
 
-    desc "language LANGUAGE_NAME", "Selects the language you'd like to receive notifications in."
+    desc "language LANGUAGE_NAME", I18n.t("tasks.set.language.desc")
     def language(language_name)
       client.settings(:lang => language_name)
-      say "@#{@rcfile.active_profile[0]}'s language has been updated."
+      say I18n.t("tasks.set.language.success", :profile => @rcfile.active_profile[0])
     end
 
-    desc "location PLACE_NAME", "Updates the location field in your profile."
+    desc "location PLACE_NAME", I18n.t("tasks.set.location.desc")
     def location(place_name)
       client.update_profile(:location => place_name)
-      say "@#{@rcfile.active_profile[0]}'s location has been updated."
+      say I18n.t("tasks.set.location.success", :profile => @rcfile.active_profile[0])
     end
 
-    desc "name NAME", "Sets the name field on your Twitter profile."
+    desc "name NAME", I18n.t("tasks.set.name.desc")
     def name(name)
       client.update_profile(:name => name)
-      say "@#{@rcfile.active_profile[0]}'s name has been updated."
+      say I18n.t("tasks.set.name.success", :profile => @rcfile.active_profile[0])
     end
 
-    desc "url URL", "Sets the URL field on your profile."
+    desc "url URL", I18n.t("tasks.set.url.desc")
     def url(url)
       client.update_profile(:url => url)
-      say "@#{@rcfile.active_profile[0]}'s URL has been updated."
+      say I18n.t("tasks.set.url.success", :profile => @rcfile.active_profile[0])
     end
 
   end
